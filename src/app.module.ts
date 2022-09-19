3096ac54262548c86d7e8dda8f83e2e18fcb761d
Artur <artur2000sis@gmail.com>
2022-09-19 10:03:03 +0300

    connect BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,46 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryesModule } from './categoryes/categoryes.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CategoryeEntity } from './categoryes/entities/categorye.entity';
+import { config } from 'dotenv';
 
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: 'localhost',
+//       port: 5432,
+//       username: 'root',
+//       password: 'root',
+//       database: 'pizza-res',
+//       entities: [CategoryeEntity],
+//       synchronize: true,
+//     }),
+//     CategoryesModule,
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
+config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      host: process.env.POSTGRES_HOST,
       port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'pizza-res',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       entities: [CategoryeEntity],
       synchronize: true,
     }),
